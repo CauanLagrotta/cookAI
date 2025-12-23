fbaeb1ad0078b258153186326ac4eaa36c91ac77
CauanLagrotta <cauanlagrotta.dev@gmail.com>
2025-12-22 21:01:22 -0300

    build: initial setup

---
+++ src/main/java/com/cookai/CookaiApplication.java
@@ -0,0 +1,13 @@
+package com.cookai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cookai/CookaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cookai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
